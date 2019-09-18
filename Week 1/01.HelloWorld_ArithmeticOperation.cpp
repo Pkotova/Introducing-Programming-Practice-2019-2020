137bffa51e34254a17eccb9a4549dc74eb323424
Pkotova <petya.kotova@abv.bg>
2019-09-18 18:43:19 +0300

    Salutions

--- Week 1/01.HelloWorld_ArithmeticOperation.cpp
+++ Week 1/01.HelloWorld_ArithmeticOperation.cpp
@@ -17,18 +17,18 @@ int main()
 {
 	cout << "Hello World" << endl;
 
-	//Declaration of integer 
+//Declaration of integer 
 	int integer;
 
-	//Initialization
+//Initialization
 	integer = 124;
 
-	//literal
+//literal
 	const double e = 2.7182; 
 	const double pi = 3.1415;
 	const double fi = (1 + sqrt(5)) / 2;
 
-	//data types 
+//data types 
 	int myInt = 1;
 
 	double doubleNum = 1.2;
@@ -45,18 +45,18 @@ int main()
 	char dolarSymbol = '&';
 	char hashTag = '#'; // followForFollow
 
-	//Input/Output of console 
+//Input/Output of console 
 	int favouriteNumber;
 	cout << "My favourite number is: ";
 	cin >> favouriteNumber;
 	cout << "Here is my favourite number! " << favouriteNumber <<endl;
 
-	// Displaying and not displaying symbols
+// Displaying and not displaying symbols
 	cout << "\n this is new line";
 	cout << endl;
 	cout << "\t this is tabulation";
 
-	//Arithmetic Operations 
+//Arithmetic Operations 
 	double sum = 1 + 12;
 	cout << sum << endl; 
 
@@ -72,10 +72,21 @@ int main()
 	double modDevision = 15 % 2; // 15 % 2 = 0.5;
 	cout << modDevision << endl;
 
-	//Operators priority (Arithmetic)
-	//(), *, / ,% ,+ ,- 
+//Operators priority (Arithmetic)
+//(), *, / ,% ,+ ,- 
 	double result = ((5 - 16) * 13 / 2) % 14 - 154 * 3 + 54 % 2;
 	cout << result << endl;
 
+// cmath functions good to know
+	cout << sin(90) << endl;		//all trigonometric functions
+	cout << log(10) << endl;		//natural logarithm
+	cout << log10(10) << endl;		// common logarithm
+	cout << pow(2, 10) << endl;	    //power 2 * 2 * 2 * 2 .... 
+	cout << sqrt(69) << endl;		//square of number 
+	cout << ceil(13.23) << endl;	//round up value
+	cout << floor(134.25) << endl;	//round down value
+	cout << round(5.49) << endl;	//round to nearest
+	cout << fabs(-123.23) << endl;	//abosolute value double numbers
+	cout << abs(-123) << endl;		//absolute value integer numbers
 }
 
