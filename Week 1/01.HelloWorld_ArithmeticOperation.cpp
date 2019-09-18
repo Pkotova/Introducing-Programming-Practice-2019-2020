0f578b832e5a014e731cf10787cdb5861df9dee5
Pkotova <petya.kotova@abv.bg>
2019-09-18 17:15:46 +0300

    Examples

---
+++ Week 1/01.HelloWorld_ArithmeticOperation.cpp
@@ -0,0 +1,81 @@
+//Petya Kotova
+//Daniel Shahanski
+/*
+	Sofia University Faculty of Mathematics and Informatics
+	1st course 
+	Information Systems
+	2nd group
+*/
+// Start the program with ctrt + F5 
+
+#include "pch.h"
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+
+	//Declaration of integer 
+	int integer;
+
+	//Initialization
+	integer = 124;
+
+	//literal
+	const double e = 2.7182; 
+	const double pi = 3.1415;
+	const double fi = (1 + sqrt(5)) / 2;
+
+	//data types 
+	int myInt = 1;
+
+	double doubleNum = 1.2;
+	float floatNum = 1.23;
+
+	bool iamSmart = false; //poor you ;(
+	bool iamAFool = 0; 
+	bool iamPretty = true;
+	bool isHandsome = 1;
+
+	char letter = 'a';
+	char number = '2';
+	char percentSymbol = '%';
+	char dolarSymbol = '&';
+	char hashTag = '#'; // followForFollow
+
+	//Input/Output of console 
+	int favouriteNumber;
+	cout << "My favourite number is: ";
+	cin >> favouriteNumber;
+	cout << "Here is my favourite number! " << favouriteNumber <<endl;
+
+	// Displaying and not displaying symbols
+	cout << "\n this is new line";
+	cout << endl;
+	cout << "\t this is tabulation";
+
+	//Arithmetic Operations 
+	double sum = 1 + 12;
+	cout << sum << endl; 
+
+	double subtraction = 14 - 154;
+	cout << subtraction << endl;
+
+	double product = 13 * 123;
+	cout << product << endl;
+
+	double devision = 15 / 2; // 15 / 2 = 7
+	cout << devision << endl;
+
+	double modDevision = 15 % 2; // 15 % 2 = 0.5;
+	cout << modDevision << endl;
+
+	//Operators priority (Arithmetic)
+	//(), *, / ,% ,+ ,- 
+	double result = ((5 - 16) * 13 / 2) % 14 - 154 * 3 + 54 % 2;
+	cout << result << endl;
+
+}
+
